c110de8945a3c4b8c60b40c30305bcae6ca4de94
Karthik Karanth <karanth.karthik@gmail.com>
2020-02-24 02:04:14 -0700

    switch from taichi to pycuda+pygame

--- automata.py
+++ automata.py
@@ -1,73 +1,51 @@
-import taichi as ti
+import pygame
+import numpy as np
+import pycuda.driver as cuda
+import pycuda.autoinit
+import pycuda.gpuarray as gpuarray
+from pycuda.compiler import SourceModule
 
-ti.init(arch=ti.cuda, print_ir=True) # Run on GPU by default
-# ti.set_logging_level(ti.TRACE)
+N = 350
 
-n = 200
-cells = ti.var(dt=ti.i32, shape=(n, n))
+pygame.init()
+display = pygame.display.set_mode((N, N))
 
-img_n = 700
-pixels = ti.var(dt=ti.f32, shape=(img_n, img_n))
+cell_state = np.zeros((N, N), dtype=np.int32)
+cell_state[0, N//2] = 1
+mod = SourceModule('''
+  __global__ void cell_step(int *A, int step, int n)
+  {
+    const int idx = threadIdx.x;
+    const int a = A[(step-1)*n + idx - 1];
+    const int b = A[(step-1)*n + idx];
+    const int c = A[(step-1)*n + idx + 1];
+    const int p = a ^ (b | c);
 
-@ti.kernel
-def gen_image():
-    for i, j in pixels:
-        x = (n * i) // img_n
-        y = (n * j) // img_n
-        pixels[i, j] = cells[x, y]
+    A[step * n + idx] = p;
+  }
+  ''')
+func = mod.get_function('cell_step')
 
-@ti.kernel
-def init_cells():
-    for i, j in cells:
-        cells[i, j] = 0
+step = 0
+def update():
+    global step
+    step += 1
 
-    cells[0, n//2] = 1
+    if step >= N:
+        return cell_state
 
-@ti.func
-def rule_30(a: ti.i32, b: ti.i32, c: ti.i32) -> ti.i32:
-    p = 0
-    if a == 1:
-        if b == 0 and c == 0:
-            p = 1
-        else:
-            p = 0
-    else:
-        if b == 0 and c == 0:
-            p = 0
-        else:
-            p = 1
-    return p
+    func(cuda.InOut(cell_state), np.int32(step), np.int32(N), block=(N, 1, 1))
 
-@ti.func
-def rule_110(a: ti.i32, b: ti.i32, c: ti.i32) -> ti.i32:
-    p = 0
-    if a == 1 and b == 1 and c == 1:
-        p = 0
-    elif a == 1 and b == 0 and c == 0:
-        p = 0
-    elif a == 0 and b == 0 and c == 0:
-        p = 0
-    else:
-        p = 1
-    return p
+    return cell_state
 
-@ti.kernel
-def paint(t: ti.i32):
-    for i in range(1, n-1):
-        a = cells[t-1, i-1] 
-        b = cells[t-1, i] 
-        c = cells[t-1, i+1] 
 
-        cells[t, i] = rule_110(a, b, c)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    surf = pygame.surfarray.make_surface(update() * 255)
+    display.blit(surf, (0, 0))
+    pygame.display.update()
 
-gui = ti.GUI("Automata", (img_n, img_n))
-
-init_cells()
-for i in range(1, n):
-    paint(i)
-    gen_image()
-    gui.set_image(pixels)
-    gui.show()
-
-gui.set_image(pixels)
-# gui.wait_key()
+pygame.quit()
