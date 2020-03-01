877bb6bc8b870018b5f93025a9b8423d2c939076
Karthik Karanth <karanth.karthik@gmail.com>
2020-02-29 23:32:52 -0700

    docs for fb

--- fb.py
+++ fb.py
@@ -1,15 +1,42 @@
 import pygame
 
 class Viewer:
+    """
+    Represents a pygame backed frame viewer
+    """
+
     def __init__(self, update_func, display_size):
+        """
+        Parameters
+        ----------
+
+        update_func : function
+            A function that must return a numpy uint8 array with dimensions (W, H, 3)
+        display_size : tuple
+            The shape of the display window, in the form of (W, H)
+        """
+
         self.update_func = update_func
         pygame.init()
         self.display = pygame.display.set_mode(display_size)
     
     def set_title(self, title):
+        """
+        Set the title of the window
+
+        Parameters
+        ----------
+
+        title : str
+            The new title
+        """
         pygame.display.set_caption(title)
     
     def start(self):
+        """
+        Starts the display loop
+        """
+
         running = True
         while running:
             for event in pygame.event.get():
