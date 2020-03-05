a9f38720d56fb19051dbd7219798c9bc240269a1
Karthik Karanth <karanth.karthik@gmail.com>
2020-03-05 10:47:58 -0700

    use blocks

--- 04_continuous_cellular_automata.py
+++ 04_continuous_cellular_automata.py
@@ -10,8 +10,8 @@ from PIL import Image
 
 from fb import Viewer
 
-N = 400
-display_size = (900, 900)
+N = 1024*2
+display_size = (1024, 1024)
 
 cell_state = np.zeros((N, N), dtype=np.float32)
 cell_state[0, N//2] = 1
@@ -20,7 +20,7 @@ mod = SourceModule('''
     return x - floorf(x);
   }
   __global__ void cell_step(float *A, float rule, int step, int n) {
-    const int idx = threadIdx.x;
+    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
     const float a = A[(step-1)*n + idx - 1];
     const float b = A[(step-1)*n + idx];
     const float c = A[(step-1)*n + idx + 1];
@@ -32,25 +32,24 @@ mod = SourceModule('''
 func = mod.get_function('cell_step')
 
 step = 0
-rule = 0
+rule = random.random()
 last_time = time.time()
-def update():
+def update(steps=20):
     global step, rule, last_time
-    step += 1
-    curr_time = time.time()
-    dt = curr_time - last_time
-    last_time = curr_time
-    fps = 1 / dt
 
-    if step == N:
-        rule = random.random()
-        step = 1
+    for _ in range(steps):
+      step += 1
+      if step == N:
+          rule = random.random()
+          step = 1
+      func(cuda.InOut(cell_state), np.float32(rule), np.int32(step), np.int32(N),
+                block=(1024, 1, 1),
+                grid=(N//1024, 1))
 
-    func(cuda.InOut(cell_state), np.float32(rule), np.int32(step), np.int32(N), block=(N, 1, 1))
 
     image = np.stack((cell_state*255.0,)*3, axis=-1).astype('uint8')
     image = np.array(Image.fromarray(image).resize(display_size, Image.BILINEAR))
-    viewer.set_title(f'Cellular Automate: Rule {rule:.2f} [{fps:.1f} FPS]')
+    viewer.set_title(f'Cellular Automate: Rule {rule:.2f}')
     return image
 
 viewer = Viewer(update, display_size)
